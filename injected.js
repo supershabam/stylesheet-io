bee57937f8e3614eb477f7b49444861633f7fbe8
Ian Hansen <ian@supershabam.com>
2013-05-18 11:14:53 -0600

    lookin nice

--- injected.js
+++ injected.js
@@ -1,7 +1,7 @@
 <script src="/socket.io/socket.io.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-url-parser/2.2.1/purl.min.js"></script>
 <script>
-  var socket = io.connect('http://landing.stylesheet.io:80');
+  var socket = io.connect('http://landing-demo.stylesheet.io:80');
   socket.on('change', function() {
     window.location.reload();
   });
